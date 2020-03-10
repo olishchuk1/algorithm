7ded0aeceb3c9ceb1cd3baec237fee0d3d3b5044
olishchuk1 <30534182+olishchuk1@users.noreply.github.com>
2020-03-10 20:21:18 +0200

    added insertion sort

---
+++ Sortings/Insertion.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Sortings
+{
+    class Insertion
+    {
+        static void InsertionSort(int[] a)
+        {
+            for (var i = 1; i < a.Length; i++)
+            {
+                var t1 = a[i];
+                var j = i;
+
+                while (j >= 1 && a[j - 1] > t1)
+                {
+                    var t2 = a[j - 1];
+                    a[j - 1] = a[j];
+                    a[j] = t2;
+
+                    j--;
+                }
+                a[j] = t1;
+
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            int[] a = {1, 5, 2, 8, 1, 8, 1, 8, 2, 9, 3, -1, -76543, 76543, 876543, 65432, 8765432, 76543, 8765432, 765432};
+
+            foreach (var i in a)
+            {
+                Console.Write(i);
+                Console.Write(' ');
+            }
+
+            Console.WriteLine();
+
+            InsertionSort(a);
+
+            foreach (var i in a)
+            {
+                Console.Write(i);
+                Console.Write(' ');
+
+            }
+        }
+    }
+}
