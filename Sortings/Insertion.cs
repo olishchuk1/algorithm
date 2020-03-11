aa81f91cf064eb24feb130ab933170b9481974e1
olishchuk1 <30534182+olishchuk1@users.noreply.github.com>
2020-03-11 19:38:59 +0200

    added selection sort

--- Sortings/Insertion.cs
+++ Sortings/Insertion.cs
@@ -4,7 +4,28 @@ namespace Sortings
 {
     class Insertion
     {
-        static void InsertionSort(int[] a)
+        static void InsertionSortDescending(int[] a)
+        {
+            for (var i = 1; i < a.Length; i++)
+            {
+                var t1 = a[i];
+                var j = i;
+
+                while (j >= 1 && a[j - 1] < t1)
+                {
+                    var t2 = a[j - 1];
+                    a[j - 1] = a[j];
+                    a[j] = t2;
+
+                    j--;
+                }
+                a[j] = t1;
+
+            }
+
+        }
+
+        private static void InsertionSort(int[] a)
         {
             for (var i = 1; i < a.Length; i++)
             {
@@ -23,28 +44,6 @@ namespace Sortings
 
             }
 
-        }
-
-        static void Main(string[] args)
-        {
-            int[] a = {1, 5, 2, 8, 1, 8, 1, 8, 2, 9, 3, -1, -76543, 76543, 876543, 65432, 8765432, 76543, 8765432, 765432};
-
-            foreach (var i in a)
-            {
-                Console.Write(i);
-                Console.Write(' ');
-            }
-
-            Console.WriteLine();
-
-            InsertionSort(a);
-
-            foreach (var i in a)
-            {
-                Console.Write(i);
-                Console.Write(' ');
-
-            }
         }
     }
 }
---
+++ Sortings/Selection.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sortings
+{
+    class Selection
+    {
+        static void SelectionSortDescending(int[] a)
+        {
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                int minInd = i;
+
+                for (int j = i + 1; j < a.Length; j++)
+                {
+
+                    if (a[j] > a[minInd])
+                    {
+                        minInd = j;
+                    }
+                }
+                var t = a[minInd];
+                a[minInd] = a[i];
+                a[i] = t;
+            }
+        }
+        static void SelectionSort(int[] a)
+        {
+            for (var i = 0; i < a.Length - 1; i++)
+            {
+                var minInd = i;
+
+                for (var j = i + 1; j < a.Length; j++)
+                {
+
+                    if (a[j] < a[minInd])
+                    {
+                        minInd = j;
+                    }
+                }
+                var t = a[minInd];
+                a[minInd] = a[i];
+                a[i] = t;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 5, 2, 8, 1, 8, 1, 8, 2, 9, 3, -1, -76543, 76543, 876543, 65432, 8765432, 76543, 8765432, 765432 };
+
+            foreach (var i in a)
+            {
+                Console.Write(i);
+                Console.Write(' ');
+            }
+
+            Console.WriteLine();
+
+            SelectionSort(a);
+
+            foreach (var i in a)
+            {
+                Console.Write(i);
+                Console.Write(' ');
+
+            }
+        }
+    }
+}
